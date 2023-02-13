160b9a0602eb6ef77e2992d752260663b33972a5
Aman Singh <aman.singh@harness.io>
2023-02-13 09:21:06 +0530

    remove go-git deps

--- cloner/default.go
+++ cloner/default.go
@@ -6,64 +6,30 @@ package cloner
 
 import (
 	"context"
+	"fmt"
 	"io"
-	"os"
-
-	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing"
+	"os/exec"
 )
 
-// New returns a new cloner.
-func New(depth int, stdout io.Writer) Cloner {
-	return &cloner{
+// New returns a cloner that uses exec.
+func New(depth int, out io.Writer) Cloner {
+	return &execer{
 		depth:  depth,
-		stdout: stdout,
+		stdout: out,
 	}
 }
 
-// NewDefault returns a cloner with default settings.
-func NewDefault() Cloner {
-	return New(1, os.Stdout)
-}
-
 // default cloner using the built-in Git client.
-type cloner struct {
+type execer struct {
 	depth  int
 	stdout io.Writer
 }
 
 // Clone the repository using the built-in Git client.
-func (c *cloner) Clone(ctx context.Context, params Params) error {
-	opts := &git.CloneOptions{
-		RemoteName: "origin",
-		Progress:   c.stdout,
-		URL:        params.Repo,
-	}
-	// set the reference name if provided
-	if params.Ref != "" {
-		opts.ReferenceName = plumbing.ReferenceName(expandRef(params.Ref))
-	}
-	// set depth if cloning the head commit of a branch as
-	// opposed to a specific commit sha
-	if params.Sha == "" {
-		opts.Depth = c.depth
-	}
-
-	// clone the repository
-	r, err := git.PlainClone(params.Dir, false, opts)
-	if err != nil {
-		return err
-	}
-	if params.Sha == "" {
-		return nil
-	}
-
-	// checkout the sha
-	w, err := r.Worktree()
-	if err != nil {
-		return err
-	}
-	return w.Checkout(&git.CheckoutOptions{
-		Hash: plumbing.NewHash(params.Sha),
-	})
+func (c *execer) Clone(ctx context.Context, params Params) error {
+	// TODO need to set username/ password
+	cmd := exec.Command("git", "clone", fmt.Sprintf("--depth=%d", c.depth), params.Repo, params.Dir)
+	cmd.Stdout = c.stdout
+	cmd.Stderr = c.stdout
+	return cmd.Run()
 }
