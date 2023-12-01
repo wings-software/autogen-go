ef9b650f14a6b1eb9c493210cb8f9942509d80e6
abhay084 <abhay.ganvir@harness.io>
2023-12-01 08:12:50 +0530

    rust build

--- builder/rules/harness/rule_rust.go
+++ builder/rules/harness/rule_rust.go
@@ -8,13 +8,71 @@ import (
 	"io/fs"
 
 	spec "github.com/drone/spec/dist/go"
+	"github.com/wings-software/autogen-go/utils"
 )
 
 // ConfigureRust configures a Rust step.
 func ConfigureRust(fsys fs.FS, pipeline *spec.Pipeline) error {
-	_ = pipeline.Stages[0].Spec.(*spec.StageCI)
+	stage := pipeline.Stages[0].Spec.(*spec.StageCI)
 
 	// TODO
+
+	// check for the cargo.toml file
+	if !utils.Exists(fsys, "Cargo.toml") {
+		return nil
+	}
+
+	// check if we should use a container-based
+	// execution environment.
+	useImage := utils.IsContainerRuntime(pipeline)
+
+	// add cargo build step
+	{
+		script := new(spec.StepExec)
+		script.Run = "cargo build"
+
+		if useImage {
+			script.Image = "rust"
+		}
+
+		step := new(spec.Step)
+		step.Name = "cargo_build"
+		step.Type = "script"
+		step.Spec = script
+
+		stage.Steps = append(stage.Steps, step)
+	}
+
+	// add the cargo test step
+	{
+		script := new(spec.StepExec)
+		script.Run = "cargo test "
+
+		if useImage {
+			script.Image = "rust"
+		}
+
+		step := new(spec.Step)
+		step.Name = "cargo_test"
+		step.Type = "script"
+		step.Spec = script
+
+		stage.Steps = append(stage.Steps, step)
+	}
+
+	// add docker build and push step
+	{
+		step := new(spec.Step)
+		step.Name = "docker build push"
+		step.Type = "BuildAndPushDockerRegistry"
+		script := map[string]interface{}{
+			"tags": "latest",
+			"repo": "harness-test",
+		}
+		step.Spec = script
+
+		stage.Steps = append(stage.Steps, step)
+	}
 
 	return nil
 }
